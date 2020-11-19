b9b1e35e6889b4da472fa128ce92fee231d5a056
Vladimir <qe.wtsn@gmail.com>
2020-11-19 17:39:54 +0300

    Let's the battle begingit status

---
+++ inc/ft_ls.h
@@ -0,0 +1,17 @@
+#ifndef _FT_LS_H_
+#define _FT_LS_H_
+
+#include "../libft/libft/libft.h"
+#include "../libft/inc/printf.h"
+
+typedef struct s_flags
+{
+	int		l;
+	int 	r;
+	int 	R;
+	int 	a;
+	int 	t;
+}				t_flags;
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,3 @@
+#include "inc/ft_ls.h"
+
+
